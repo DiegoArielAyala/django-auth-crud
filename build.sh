07d733fa927b907377d3ae52106b0955e8320b8d
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-05-02 12:34:03 +0200

    commented pip install

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 # Con este comando pip freeze > requirements.txt, se crea el archivo requirements.txt con todas las dependencias que necesita el proyecto. Al ejecutarlo con pip install, va a instalar todas las dependencias.
-pip install -r requirements.txt
+# pip install -r requirements.txt ya se esta ejecutando desde render.com
 
 # Convert static asset files
 python manage.py collectstatic --no-input
